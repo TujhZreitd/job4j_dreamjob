1bec5c4b2247581b12b419beaec61360993fc604
TujhZreitd <egor.yakushev.96@bk.ru>
2024-09-30 16:26:57 +0300

    1. Страница login.html [#504863]

--- src/main/java/ru/job4j/dreamjob/controller/UserController.java
+++ src/main/java/ru/job4j/dreamjob/controller/UserController.java
@@ -29,6 +29,20 @@ public class UserController {
             model.addAttribute("message", "Пользователь с такой почтой существует");
             return "errors/404";
         }
+    }
 
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user, Model model) {
+        var userOptional = userService.findByEmailAndPassword(user.getEmail(), user.getPassword());
+        if (userOptional.isEmpty()) {
+            model.addAttribute("error", "Почта или пароль введены неверно");
+            return "users/login";
+        }
+        return "redirect:/vacancies";
     }
 }
